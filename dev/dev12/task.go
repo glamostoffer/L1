8fb5be83a292d2fc845adaf3ae69ae39604f7568
glamostoffer <glamostoffer@gmail.com>
2023-12-02 12:27:09 +0300

    add task 12

--- dev/dev12/task.go
+++ dev/dev12/task.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+// TODO: сделать сет потокобезопасным и с дженериками???
+
+func main() {
+	sequence := []string{"cat", "cat", "dog", "cat", "tree", "bool"}
+	// set - сет строк
+	set := make(map[string]struct{})
+
+	// добавляем элементы в свою реализацию сета
+	for _, s := range sequence {
+		set[s] = struct{}{}
+	}
+
+	fmt.Println(set)
+	for k := range set {
+		fmt.Println(k)
+	}
+
+	// удаление из сета
+	delete(set, "bool")
+
+	fmt.Println(set)
+	for k := range set {
+		fmt.Println(k)
+	}
+}
